6c825cc7c8853c7498a6dffae37332a63b87b693
Amit Uttam <amituttam82@gmail.com>
2011-05-01 02:19:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoptilludrop::Application.config.secret_token = '140d528c1cd0559c50b1a3257605dd3a0e2228ebf4fbf3cbe6a99c5f101c7cd008cd017d77ec0e373e7834ec1c9d4f01bc38339b7b1b3429ccb45a0cd41c638b'
